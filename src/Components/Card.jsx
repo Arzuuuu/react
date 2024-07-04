a187ccd5d13ca51e590963d0ad9b15052fdfe918
Arzuuuu <arzu22786@gmail.com>
2024-07-04 15:15:50 +0530

    first

---
+++ src/Components/Card.jsx
@@ -0,0 +1,15 @@
+// Card.jsx
+import React from 'react';
+
+function Card() {
+  return (
+    <div className='w-full h-screen bg-zinc-200 flex justify-center items-center'>
+      <div className='px-3 py-4 bg-zinc-100'>
+       
+        Hello, this is a card.
+      </div>
+    </div>
+  );
+}
+
+export default Card;
